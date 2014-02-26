5bcd776e206e51964d0df1a68c83a96a6b2f24ed
Richard Wilson <richard.wilson@smgdesign.org>
2014-02-26 12:22:42 +0000

    Added the table in even if no orders have been placed, to enable the polling function to correctly display orders

--- source/app/views/web/index.php
+++ source/app/views/web/index.php
@@ -1,6 +1,4 @@
-<?php
-if (!empty($orders)) {
-    ?>
+
 <table cellpadding="14" cellspacing="0" class="order_list">
     <thead>
         <tr>
@@ -13,25 +11,24 @@ if (!empty($orders)) {
     </thead>
     <tbody>
         <?php
-        foreach ($orders as $id=>$order) {
-            ?>
-        <tr class="status_<?php echo $order['status']; ?>">
-            <td align="center" width="60" class="order_id"><?php echo $id; ?></td>
-            <td align="center" width="100"><?php echo $order['table']; ?></td>
-            <td class="order_status_select"><input type="hidden" name="order_id" value="<?php echo $id; ?>" /><select name="order_status">
-                    <?php
-                    echo $this->selectList($this->orderStatuses(), $order['status']);
-                    ?>
-                </select>
-            </td>
-            <td><?php echo $order['time_ordered']; ?></td>
-            <td align="center" width="150"><a href="/web/view/<?php echo $id; ?>" class="view_order_btn">View Order #<?php echo $id; ?></a></td>
-        </tr>
+        if (!empty($orders)) {
+            foreach ($orders as $id=>$order) {
+                ?>
+            <tr class="status_<?php echo $order['status']; ?>">
+                <td align="center" width="60" class="order_id"><?php echo $id; ?></td>
+                <td align="center" width="100"><?php echo $order['table']; ?></td>
+                <td class="order_status_select"><input type="hidden" name="order_id" value="<?php echo $id; ?>" /><select name="order_status">
+                        <?php
+                        echo $this->selectList($this->orderStatuses(), $order['status']);
+                        ?>
+                    </select>
+                </td>
+                <td><?php echo $order['time_ordered']; ?></td>
+                <td align="center" width="150"><a href="/web/view/<?php echo $id; ?>" class="view_order_btn">View Order #<?php echo $id; ?></a></td>
+            </tr>
         <?php
+            }
         }
         ?>
     </tbody>
 </table>
-<?php
-}
-?>
